4f602ae762e54c7ee09b3421c33325e321abd238
Paolo Flores <pflores.fisi22@gmail.com>
2023-10-17 20:36:09 -0500

    feat: add prisma client connection

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
@@ -5,6 +6,7 @@ import { router } from "./router";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
@@ -13,3 +15,25 @@ app.use(router);
 app.listen(PORT, () => {
   console.log(`App listening on port: ${PORT}`);
 });
+
+async function main() {
+  // const user = await prisma.users.create({
+  //   data: {
+  //     email: "damaris@ieee.org",
+  //     hashed_password: "******",
+  //     names: "Damaris Marian",
+  //     surnames: "Del Carpio Martinez",
+  //   },
+  // });
+  // console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
