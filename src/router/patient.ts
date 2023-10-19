485e90347f23f72b6b14608bfbc481d6181ecb1c
damarizz <138645520+damarizz@users.noreply.github.com>
2023-10-19 00:58:59 -0500

    feat: validation of fields in POST and PUT methods

--- src/router/patient.ts
+++ src/router/patient.ts
@@ -18,14 +18,71 @@ router.get("/two", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const patient = await prisma.patient.create({ data: req.body });
-  res.send(patient);
-  console.log(req.body);
+  try {
+    const { names, surnames, documentType, document, gender } = req.body;
+
+    if (
+      typeof names !== "string" ||
+      names.trim() === "" ||
+      typeof surnames !== "string" ||
+      surnames.trim() === "" ||
+      typeof documentType !== "string" ||
+      documentType.trim() === "" ||
+      typeof document !== "string" ||
+      document.trim() === "" ||
+      typeof gender !== "string" ||
+      gender.trim() === ""
+    ) {
+      res.sendStatus(400);
+    } else {
+      const patient = await prisma.patient.create({ data: req.body });
+      res.send(patient);
+    }
+    console.log(req.body);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
-router.put("/:id", (req, res) => {
-  res.setHeader("content-type", "text/plain");
-  res.send("editing patient no. " + req.params.id);
+router.put("/:id", async (req, res) => {
+  try {
+    const { names, surnames, documentType, document, gender } = req.body;
+    const patientId = parseInt(req.params.id);
+
+    if (
+      typeof names !== "string" ||
+      names.trim() === "" ||
+      typeof surnames !== "string" ||
+      surnames.trim() === "" ||
+      typeof documentType !== "string" ||
+      documentType.trim() === "" ||
+      typeof document !== "string" ||
+      document.trim() === "" ||
+      typeof gender !== "string" ||
+      gender.trim() === ""
+    ) {
+      res.sendStatus(400);
+    } else {
+      const updatedPatient = await prisma.patient.update({
+        where: {
+          id: patientId,
+        },
+        data: {
+          names,
+          surnames,
+          documentType,
+          document,
+          gender,
+        },
+      });
+      res.send(updatedPatient);
+      console.log(updatedPatient);
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 export { router };
