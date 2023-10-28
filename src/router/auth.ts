83bf50a3e0dffb0a663b665c6a0f9b59c60e3719
Paolo Flores <pflores.fisi22@gmail.com>
2023-10-28 02:58:45 -0500

    feat: add /auth/profile endpoint

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -3,6 +3,7 @@ import { userLoginSchema } from "../schemas/user.schema";
 import { sha256 } from "../utils";
 import { Router } from "express";
 import { prisma } from "../db";
+import { authWithUserMiddleware } from "../middlewares/auth";
 
 const router = Router();
 
@@ -31,7 +32,6 @@ router.post("/login", async (req, res) => {
     return;
   }
 
-  // payload
   const token = jwt.sign(
     {
       user_id: userLogin.id,
@@ -47,37 +47,8 @@ router.post("/login", async (req, res) => {
   });
 });
 
-router.get("/user", async (req, res) => {
-  const users = await prisma.users.findMany({
-    include: {
-      roles: true,
-    },
-  });
-
-  res.send(users);
-});
-
-router.get("/users/:id", async (req, res) => {
-  const user_id = Number(req.params.id);
-
-  if (isNaN(user_id) || user_id <= 0) {
-    res.sendStatus(400);
-    return;
-  }
-  try {
-    const user = await prisma.users.findFirst({
-      where: {
-        id: Number(req.params.id),
-      },
-    });
-    if (user != null) {
-      res.send(user);
-    } else {
-      res.sendStatus(404);
-    }
-  } catch (error) {
-    res.sendStatus(500);
-  }
-});
+router.get("/profile", authWithUserMiddleware, (req, res) =>
+  res.send(req.user),
+);
 
 export { router };
