97bea2abaeb2c1db2b221a2b85863022afe3fe9f
Paolo Flores <pflores.fisi22@gmail.com>
2023-11-15 00:49:36 -0500

    hotfix: downgrade chalk to 4.18.2 since it didn't support ES modules
    Digital Ocean logs:
    
    [2023-11-15 04:34:01] Error [ERR_REQUIRE_ESM]: require() of ES Module /workspace/node_modules/chalk/source/index.js from /workspace/dist/src/docs/register.js not supported.
    [2023-11-15 04:34:01] Instead change the require of index.js in /workspace/dist/src/docs/register.js to a dynamic import() which is available in all CommonJS modules.
    [2023-11-15 04:34:01]     at Object.<anonymous> (/workspace/dist/src/docs/register.js:8:33)
    [2023-11-15 04:34:01]     at Object.<anonymous> (/workspace/dist/src/docs/handler.js:8:20)
    [2023-11-15 04:34:01]     at Object.<anonymous> (/workspace/dist/src/router/index.js:38:19)
    [2023-11-15 04:34:01]     at Object.<anonymous> (/workspace/dist/src/server.js:10:18) {
    [2023-11-15 04:34:01]   code: 'ERR_REQUIRE_ESM'
    [2023-11-15 04:34:01] }

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -4,51 +4,91 @@ import { sha256 } from "../utils";
 import { Router } from "express";
 import { prisma } from "../db";
 import { authWithUserMiddleware } from "../middlewares/auth";
+import { docs } from "../docs/middleware";
+import { z } from "zod";
 
 const router = Router();
 
 const ONE_MONTH = 30 * 24 * 60 * 60;
 
-router.post("/login", async (req, res) => {
-  const validCredential = userLoginSchema.safeParse(req.body);
-  if (!validCredential.success) {
-    res.status(400).send({
-      message: "Credenciales inválidas :(",
+router.post(
+  "/login",
+  async (req, res) => {
+    const validCredential = userLoginSchema.safeParse(req.body);
+    if (!validCredential.success) {
+      res.status(400).send({
+        message: "Credenciales inválidas :(",
+      });
+      return;
+    }
+
+    const userLogin = await prisma.user.findUnique({
+      where: {
+        email: validCredential.data.email,
+        hashed_password: sha256(validCredential.data.password),
+      },
     });
-    return;
-  }
 
-  const userLogin = await prisma.user.findUnique({
-    where: {
-      email: validCredential.data.email,
-      hashed_password: sha256(validCredential.data.password),
-    },
-  });
+    if (userLogin === null) {
+      res.status(404).send({
+        message: "email or password do not exist!!",
+      });
+      return;
+    }
 
-  if (userLogin === null) {
-    res.status(404).send({
-      message: "email or password do not exist!!",
+    const token = jwt.sign(
+      {
+        user_id: userLogin.id,
+      },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: ONE_MONTH,
+      },
+    );
+
+    res.send({
+      token: token,
     });
-    return;
-  }
-
-  const token = jwt.sign(
-    {
-      user_id: userLogin.id,
+  },
+  docs({
+    body: userLoginSchema,
+    responses: {
+      200: {
+        schema: z.object({
+          token: z.string(),
+        }),
+      },
     },
-    process.env.JWT_SECRET as string,
-    {
-      expiresIn: ONE_MONTH,
-    },
-  );
+  }),
+);
 
-  res.send({
-    token: token,
-  });
+const userResponseSchema = z.object({
+  email: z.string(),
+  id: z.number(),
+  names: z.string(),
+  surnames: z.string(),
+  roles: z.array(
+    z.object({
+      id: z.number(),
+      name: z.string(),
+    }),
+  ),
 });
 
-router.get("/profile", authWithUserMiddleware, (req, res) =>
-  res.send(req.user),
+router.get(
+  "/profile",
+  authWithUserMiddleware,
+  (req, res) => res.send(req.user),
+  docs({
+    description: `Recibe un token a través de un header de autentiación, y devuelve el usuario asociado a él.
+
+El token se envía a través del header \`Authorization: Bearer <token>\``,
+    responses: {
+      200: {
+        schema: userResponseSchema,
+      },
+    },
+  }),
 );
 
 export { router };
